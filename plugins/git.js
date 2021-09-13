a3b9fac6ea21d2f045a9d6d8db4a63420057f597
COOL1BOT1 <90600570+COOL1BOT1@users.noreply.github.com>
2021-09-14 01:13:44 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -5,27 +5,27 @@ const axios = require('axios');
 const Language = require('../language');
 const Lang = Language.getString('wallpaper');
 
-Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message, match) => {
+Asena.addCommand({pattern: 'imash', fromMe: false, desc: Lang.WP}, (async (message, match) => {
 
     var r_text = new Array ();
     
     
    
-  r_text[0] = "https://i.imgur.com/yCYYyEJ.jpeg";
+  r_text[0] = "https://ibb.co/3h3BRyf.jpeg";
     
     
     var i = Math.floor(1*Math.random())
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*creater Amal*
-*owner number wa.me/919895828468*
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*creater Imash Nidusha*
+*owner number wa.me/94771323434*
 
-*bot setting video : https://youtu.be/RlSG4ZgPls0*
+**
 
-*githublink : https://github.com/cyberchekuthan/Amalser_v2*
+*🥶*
 
-*audio commads : https://github.com/cyberchekuthan/Amalser_v2/tree/master/uploads*
+*iMASH BOT IS THE NEW SRI LANKAN BOT*
 `}) 
 
 }));
